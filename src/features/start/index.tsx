9ffee58e3c509e6c91c9155dd85e39c89c06bc25
jungsikjeong <wndtlr1024@gmail.com>
2025-12-28 20:42:22 +0900

    feat: 초기 튜토리얼 버니 캐릭터 회전하는거 완성

--- src/features/start/index.tsx
+++ src/features/start/index.tsx
@@ -1,3 +1,71 @@
+import { Application, extend, useTick } from "@pixi/react";
+import { Assets, Container, Sprite, Texture } from "pixi.js";
+import { useState, useEffect } from "react";
+extend({
+  Container,
+  Sprite,
+});
+
 export default function Start() {
-  return <div>Start</div>;
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <Application
+      width={dimensions.width}
+      height={dimensions.height}
+      backgroundColor={0x1099bb}
+    >
+      <pixiContainer>
+        <Bunny dimensions={dimensions} />
+      </pixiContainer>
+    </Application>
+  );
+}
+
+function Bunny({
+  dimensions,
+}: {
+  dimensions: { width: number; height: number };
+}) {
+  const [bunny, setBunny] = useState(Texture.EMPTY);
+  const [rotation, setRotation] = useState(0);
+
+  useEffect(() => {
+    if (bunny === Texture.EMPTY) {
+      Assets.load("https://pixijs.com/assets/bunny.png").then(
+        (loadedTexture) => {
+          setBunny(loadedTexture);
+        }
+      );
+    }
+  }, [bunny]);
+
+  useTick((time) => {
+    setRotation(rotation + 0.1 * time.deltaTime);
+  });
+
+  return (
+    <pixiSprite
+      texture={bunny}
+      x={dimensions.width / 2}
+      y={dimensions.height / 2}
+      anchor={{ x: 0.5, y: 0.5 }}
+      rotation={rotation}
+    />
+  );
 }
